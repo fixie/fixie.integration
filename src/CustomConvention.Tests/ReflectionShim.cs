3066b722e8bb524f81f2f50aa179629134d6661d
Patrick Lioi <patrick.lioi@gmail.com>
2017-08-14 13:57:45 -0500

    The solution includes a mix of .NET Framework-targeting test projects and multitargeting test projects in order to verify Fixie's .NET Core support.

---
+++ src/CustomConvention.Tests/ReflectionShim.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+
+namespace CustomConvention.Tests
+{
+    public static class ReflectionShim
+    {
+        public static T GetCustomAttribute<T>(this Type type) where T : Attribute
+            => type.GetTypeInfo().GetCustomAttribute<T>(inherit: true);
+    }
+}
