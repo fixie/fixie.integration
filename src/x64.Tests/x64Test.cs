ea9032c64e5bf85f7b1718adeb79d9b6d7b6175d
Patrick Lioi <patrick.lioi@gmail.com>
2017-08-18 16:31:52 -0500

    32- and 64-bit test projects demonstrate that they are truly executed with the expected bitness.

--- src/x64.Tests/x64Test.cs
+++ src/x64.Tests/x64Test.cs
@@ -1,9 +1,13 @@
 ﻿namespace x64.Tests
 {
+    using System;
+    using Shouldly;
+
     class x64Tests
     {
         public void ShouldRunTestsInAssembliesTargetingx64()
         {
+            (IntPtr.Size * 8).ShouldBe(64);
         }
     }
 }
--- src/x86.Tests/x86Tests.cs
+++ src/x86.Tests/x86Tests.cs
@@ -1,9 +1,13 @@
 ﻿namespace x86.Tests
 {
+    using System;
+    using Shouldly;
+
     class x86Tests
     {
         public void ShouldRunTestsInAssembliesTargetingx86()
         {
+            (IntPtr.Size * 8).ShouldBe(32);
         }
     }
 }
