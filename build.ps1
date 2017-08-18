99ad0639e124ebf0a47dbb00fe09da8aa8f769ac
Patrick Lioi <patrick.lioi@gmail.com>
2017-08-18 16:35:48 -0500

    Now that Fixie test projects are executables, and now that `dotnet fixie` therefore no longer has an -x86 option, the build script demonstrates that `dotnet fixie` can execute tests under the correct bitness of each individual test assembly.

--- build.ps1
+++ build.ps1
@@ -21,17 +21,12 @@ function Build {
 }
 
 function Test {
-
     $test = { dotnet fixie --configuration $configuration --no-build }
-    $test_x86 = { dotnet fixie --configuration $configuration --no-build --x86 }
 
     run-tests src/DefaultConvention.Tests $test 2
     run-tests src/CustomConvention.Tests $test 0
     run-tests src/x64.Tests $test 0
-
-    run-tests src/DefaultConvention.Tests $test_x86 2
-    run-tests src/CustomConvention.Tests $test_x86 0
-    run-tests src/x86.Tests $test_x86 0
+    run-tests src/x86.Tests $test 0
 }
 
 run-build {
