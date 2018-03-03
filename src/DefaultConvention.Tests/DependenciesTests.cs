e0f63c9e52938ba71503569c2bf32172d7d75691
Patrick Lioi <patrick.lioi@gmail.com>
2018-03-03 13:20:49 -0600

    To help reproduce and diagnose binding redirect issues in the future, include a test which references an old version of the popular Newtonsoft.Json package and assert that Fixie's own transitive dependency on it doesn't interfere.

---
+++ src/DefaultConvention.Tests/DependenciesTests.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Shouldly;
+
+namespace DefaultConvention.Tests
+{
+    class DependenciesTests
+    {
+        public void CanLoadExpectedVersionOfDependentAssembly()
+        {
+            //For .NET Core test assemblies, Fixie brings in a transitive
+            //dependency for Newtonsoft.Json.
+
+            //Assert that the version in effect at runtime is the one
+            //explicitly referenced by the Fixie.Integration project in
+            //this solution.
+
+            typeof(JsonConvert).Assembly.GetName().Version.ToString()
+                .ShouldBe("10.0.0.0");
+
+            var sample = JsonConvert.DeserializeObject<Sample>("{Property:\"Value\"}");
+            sample.Property.ShouldBe("Value");
+        }
+
+        class Sample
+        {
+            public string Property { get; set; }
+        }
+    }
+}
