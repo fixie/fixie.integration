a8308866859230d8872b0a39749b58e35938fdd2
Patrick Lioi <patrick.lioi@gmail.com>
2018-10-17 18:44:09 -0500

    Simplify heading outputs as all build step names are single words.

--- build-helpers.ps1
+++ build-helpers.ps1
@@ -51,13 +51,11 @@ function exec($cmd) {
 
 function step($block) {
     $command = $block.ToString().Trim()
-    heading $command
-    &$block
-}
 
-function heading($title) {
     write-host
-    write-host $title.Replace("-", " ") -fore CYAN
+    write-host $command -fore CYAN
+
+    &$block
 }
 
 function main($mainBlock) {
