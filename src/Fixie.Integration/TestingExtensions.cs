f1316887f12b9e51052053fe411632face791009
Patrick Lioi <patrick.lioi@gmail.com>
2021-06-12 13:27:26 -0500

    Add convenience methods shared by sample testing conventions.

---
+++ src/Fixie.Integration/TestingExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fixie.Integration
+{
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    public static class TestingExtensions
+    {
+        public static bool Has<TAttribute>(this MemberInfo member) where TAttribute : Attribute
+        {
+            return member.GetCustomAttributes<TAttribute>(true).Any();
+        }
+
+        public static async Task DisposeWhenApplicableAsync(this object instance)
+        {
+            if (instance is IAsyncDisposable asyncDisposable)
+                await asyncDisposable.DisposeAsync();
+
+            if (instance is IDisposable disposable)
+                disposable.Dispose();
+        }
+    }
+}
