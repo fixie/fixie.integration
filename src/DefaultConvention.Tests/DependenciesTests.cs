241c0ed062942fe45c2865f35e38b12ac230e4a4
Patrick Lioi <patrick.lioi@gmail.com>
2022-10-04 20:03:38 -0500

    Bump Newtonsoft.Json from 11.0.2 to 13.0.1 in response to dependabot alert.

--- src/DefaultConvention.Tests/DependenciesTests.cs
+++ src/DefaultConvention.Tests/DependenciesTests.cs
@@ -7,15 +7,14 @@
     {
         public void CanLoadExpectedVersionOfDependentAssembly()
         {
-            //For .NET Core test assemblies, Fixie brings in a transitive
-            //dependency for Newtonsoft.Json.
+            //Fixie brings in a transitive, often old, dependency on Newtonsoft.Json.
 
             //Assert that the version in effect at runtime is the one
             //explicitly referenced by the Fixie.Integration project in
             //this solution.
 
             typeof(JsonConvert).Assembly.GetName().Version.ToString()
-                .ShouldBe("11.0.0.0");
+                .ShouldBe("13.0.0.0");
 
             var sample = JsonConvert.DeserializeObject<Sample>("{Property:\"Value\"}");
             sample.Property.ShouldBe("Value");
